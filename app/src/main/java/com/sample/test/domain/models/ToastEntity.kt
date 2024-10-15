c1ebb45ae4d8eeb285b883622e8dd6340b0202fd
ash <anupam.singh@csem.ch>
2024-10-16 00:32:09 +0200

    Test Template

--- app/src/main/java/com/sample/test/domain/models/ToastEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.sample.test.domain.models
-
-data class ToastEntity(
-    val name: String? = null,
-    val id: Int = 0
-)
--- app/src/main/java/com/sample/test/domain/repository/ToastCatalogListRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.sample.test.domain.repository
-
-import com.sample.test.data.models.ToastEntityDto
-
-interface ToastCatalogListRepository {
-    suspend fun getToastList(): Result<List<ToastEntityDto>>
-}
